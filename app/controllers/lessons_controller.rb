76d54936fb4b05ef3b2967268026385014a6e656
Takehiro Mouri <takehiromouri@gmail.com>
2015-10-07 14:55:12 +0000

    Added authentication for lesson show page

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,18 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :authenticate_enrollment_for_current_user
+
 	def show
 	end
 
 	private
+
+
+	def authenticate_enrollment_for_current_user
+		if current_user.enrolled_in?(current_lesson.section.course) == false
+			redirect_to course_path, :alert => 'Enroll in this course to view lessons'
+		end
+	end
 
 	helper_method :current_lesson
   	def current_lesson
